ec1c234d457c92adca64fbe56bb5e1e2a314fb63
Ryan Murzyn <orian3737@gmail.com>
2024-06-24 17:24:27 -0400

    yea

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello Programmer")
 
 def greet(name):
-    pass
+    print(f"Hi there, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hi there, {name}!")
 
 def add(num1, num2):
-    pass
+    print(num1 + num2)
 
 def halve(number):
-    pass
+    result = number / 2
+    return result
+
+
+greet_programmer()
+greet("Ryan")
+greet_with_default()
+greet_with_default("joe")
+add(1, 2)
+halve(8)
